f0a3fe8bf3c32f9f33bf535f010bfc34e17d6981
NhatNamNQ <138838322+NhatNamNQ@users.noreply.github.com>
2025-02-28 13:59:28 +0000

    Initial commit: CV app with Vite, React, Tailwind, and Shadcn

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <button>Click</button>
+    </div>
+  )
+}
+
+export default App
