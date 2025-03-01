fbe6fd27de67ed7981cc97a6f610fb2afee36ae0
NhatNamNQ <138838322+NhatNamNQ@users.noreply.github.com>
2025-03-01 03:17:15 +0000

    [ADD] phone validation

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -10,14 +10,29 @@ function GeneralInfo({
     onEdit
 }) {
     const [formData, setFormData] = useState(data);
+    const [phoneError, setPhoneError] = useState('');
+
+    const phoneRegex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        if (!phoneRegex.test(formData.phone)) {
+            setPhoneError("Please enter a valid phone number");
+            return;
+        }
         onSubmit(formData)
     }
 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value })
+
+        if (e.name === "phone") {
+            if (!phoneRegex.test(e.target.value) && e.target.value.length > 0) {
+                setPhoneError("Please enter a valid phone number");
+            } else {
+                setPhoneError('');
+            }
+        }
     }
 
     return (
@@ -49,17 +64,18 @@ function GeneralInfo({
                         name='phone'
                         onChange={handleChange}
                         value={formData.phone}
+                        className={phoneError ? 'border-red-500' : ''}
                     />
                     <Button type='submit' className='mt-4'>Submit</Button>
                 </form>
-                ) : (
-                    <div className="w-full border border-black rounded p-4 my-4">
-                        <p>Name: {formData.name}</p>
-                        <p>Email: {formData.email}</p>
-                        <p>Phone: {formData.phone}</p>
-                        <Button onClick={onEdit} className='mt-4'>Edit</Button>
-                    </div>
-                )}
+            ) : (
+                <div className="w-full border border-black rounded p-4 my-4">
+                    <p>Name: {formData.name}</p>
+                    <p>Email: {formData.email}</p>
+                    <p>Phone: {formData.phone}</p>
+                    <Button onClick={onEdit} className='mt-4'>Edit</Button>
+                </div>
+            )}
         </section>
     )
 }
